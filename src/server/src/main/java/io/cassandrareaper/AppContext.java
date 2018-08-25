2a86b73c78c9d4064898ab0183b647bb72ae0160
Alexander Dejanovski <alex@thelastpickle.com>
2018-08-25 23:51:26 +0200

    Implement the sidecar mode Limit the retention time and duplicates for node operations Add sidecar integration tests Remove snapshots from sidecar integration tests
    Bump migration number to avoid conflicts

--- src/server/src/main/java/io/cassandrareaper/AppContext.java
+++ src/server/src/main/java/io/cassandrareaper/AppContext.java
@@ -46,6 +46,9 @@ public final class AppContext {
   public JmxConnectionFactory jmxConnectionFactory;
   public ReaperApplicationConfiguration config;
   public MetricRegistry metricRegistry = new MetricRegistry();
+  public String localNodeAddress;
+  public String localClusterName;
+  public String localDatacenter;
 
 
   private static class Private {
